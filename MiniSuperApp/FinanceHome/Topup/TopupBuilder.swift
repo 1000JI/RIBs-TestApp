fcf43bfef09293b847b679a0940172ffb2f479da
JIWOON CHEON <chjh1992@gmail.com>
2023-11-04 14:14:53 +0900

    Topup 리블렛 추가
    - 카드 추가 뷰를 띄울 지 충전하기 뷰를 띄울지 판단하는 뷰없는 리블렛

---
+++ MiniSuperApp/FinanceHome/Topup/TopupBuilder.swift
@@ -0,0 +1,45 @@
+//
+//  TopupBuilder.swift
+//  MiniSuperApp
+//
+//  Created by 천지운 on 11/4/23.
+//
+
+import ModernRIBs
+
+/// Topup 리블렛이 동작하기 위해 필요한 것들을 선언해두는 곳
+/// 부모 리블렛이 뷰 컨트롤러를 하나 지정해줘야 함
+protocol TopupDependency: Dependency {
+    /// Topup 리블렛이 소유하고 있는 뷰가 아니라 Topup 리블렛을 띄운 부모가 지정해준 뷰
+    var topupBaseViewController: ViewControllable { get }
+}
+
+final class TopupComponent: Component<TopupDependency> {
+
+    // TODO: Make sure to convert the variable into lower-camelcase.
+    fileprivate var topupBaseViewController: ViewControllable {
+        return dependency.topupBaseViewController
+    }
+
+    // TODO: Declare 'fileprivate' dependencies that are only used by this RIB.
+}
+
+// MARK: - Builder
+
+protocol TopupBuildable: Buildable {
+    func build(withListener listener: TopupListener) -> TopupRouting
+}
+
+final class TopupBuilder: Builder<TopupDependency>, TopupBuildable {
+
+    override init(dependency: TopupDependency) {
+        super.init(dependency: dependency)
+    }
+
+    func build(withListener listener: TopupListener) -> TopupRouting {
+        let component = TopupComponent(dependency: dependency)
+        let interactor = TopupInteractor()
+        interactor.listener = listener
+        return TopupRouter(interactor: interactor, viewController: component.topupBaseViewController)
+    }
+}
