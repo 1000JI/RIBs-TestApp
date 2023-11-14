fd9f5cb937884bf7b859ca384743c8de42e95d77
JIWOON CHEON <chjh1992@gmail.com>
2023-11-14 22:38:54 +0900

    refactoring: Integration Testing 코드 작성 완료

--- AddPaymentMethodIntegrationTests/AddPaymentMethodIntegrationTests.swift
+++ AddPaymentMethodIntegrationTests/AddPaymentMethodIntegrationTests.swift
@@ -12,6 +12,7 @@ import FinanceRepositoryTestSupport
 import AddPaymentMethodTestSupport
 import ModernRIBs
 import RIBsUtil
+import FinanceEntity
 @testable import AddPaymentMethodImp
 
 final class AddPaymentMethodIntegrationTests: XCTestCase {
@@ -21,6 +22,10 @@ final class AddPaymentMethodIntegrationTests: XCTestCase {
     private var listener: AddPaymentMethodListenerMock!
     private var viewController: UIViewController!
     private var router: Routing!
+    
+    private var repository: CardOnFileRepositoryMock {
+        dependency.cardOnFileRepository as! CardOnFileRepositoryMock
+    }
 
     override func setUpWithError() throws {
         try super.setUpWithError()
@@ -43,6 +48,39 @@ final class AddPaymentMethodIntegrationTests: XCTestCase {
     }
     
     func testAddPaymentMethod() throws {
+        // given
+        repository.addedPaymentMethod = PaymentMethod(
+            id: "1234",
+            name: "",
+            digits: "",
+            color: "",
+            isPrimary: false
+        )
+        
+        let cardNumberTF = try eventGenerator.viewWithIdentifier("addpaymentmethod_cardnumber_textfield")
+        try eventGenerator.fingerTap(at: cardNumberTF)
+        try eventGenerator.keyType("1234123412341234")
+        
+        let cvcTF = try eventGenerator.viewWithIdentifier("addpaymentmethod_security_textfield")
+        try eventGenerator.fingerTap(at: cvcTF)
+        try eventGenerator.keyType("123")
+        
+        let expiryTF = try eventGenerator.viewWithIdentifier("addpaymentmethod_expiry_textfield")
+        try eventGenerator.fingerTap(at: expiryTF)
+        try eventGenerator.keyType("1212")
+        
+        // when
+        let confirm = try eventGenerator.viewWithIdentifier("addpaymentmethod_addcard_button")
+        try eventGenerator.fingerTap(at: confirm)
+        
+        // then
+        XCTAssertEqual(repository.addCardCallCount, 1)
+        
+        // 비동기 처리 때문에 딜레이 줌
+        try eventGenerator.wait(0.2)
+        XCTAssertEqual(listener.addPaymentMethodDidAddCardCallCount, 1)
+        XCTAssertEqual(listener.addPaymentMethodDidAddCardPaymentMethod?.id, "1234")
+        
         try eventGenerator.wait(3)
     }
     
